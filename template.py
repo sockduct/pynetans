3ddc63c1c1409dd99886ab193fa4da2a2bd6de6d
James Small <james.r.small@outlook.com>
2016-04-17 13:37:50 -0700

    Commit basic Python script template

---
+++ template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+####################################################################################################
+'''
+<program description>
+'''
+
+# Imports
+#import sys
+
+# Globals
+#BASE_FILE = 'file1'
+
+__version__ = '0.0.1'
+
+
+#def func1(param1):
+    # Do stuff...
+
+if __name__ == '__main__':
+    # Do stuff...
+
