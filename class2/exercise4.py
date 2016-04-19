f251ca7353613580543683128680e812e2b69be2
James Small <james.r.small@outlook.com>
2016-04-19 16:02:29 -0700

    Initial commit of class2/exercise4

---
+++ class2/exercise4.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+####################################################################################################
+'''Connect to routers pynet-rtr1 and pynet-rtr2.  For each router, retrieve the MIB2 sysName and
+sysDescr OIDs and print them out.
+'''
+
+# Imports
+# Delete unused lines/comments!
+import snmp_helper
+import sys
+
+# Globals
+#BASE_FILE = 'file1'
+
+# Metadata
+__author__ = 'James R. Small'
+__contact__ = 'james<period>r<period>small<at>outlook<period>com'
+__date__ = 'April 19, 2016'
+__version__ = '0.0.1'
+
+
+#def func1(param1):
+    '''<description>'''
+    # Do stuff...
+
+def main():
+    '''<description>'''
+    # Do stuff...
+
+if __name__ == '__main__':
+    '''Call main and put all logic there per best practices.'''
+    sys.exit(main(sys.argv[1:]) or 0)
+
+
+####################################################################################################
+# Post coding
+#
+# pylint <script>.py
+#   Score should be >= 8.0
+#
