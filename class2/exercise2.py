152d7a312d606915371d9128ce6f6c9195f2d4a2
James Small <james.r.small@outlook.com>
2016-04-19 15:19:46 -0700

    Updated template per Beginning Python book recs, finished exercise2

--- class2/exercise2.py
+++ class2/exercise2.py
@@ -10,7 +10,7 @@ import os
 import socket
 import sys
 import telnetlib
-import time
+#import time
 import yaml
 
 # Globals
@@ -23,24 +23,27 @@ ROUTER_PROMPT = r'>|#'
 TELNET_PORT = 23
 TELNET_TIMEOUT = 5
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 def yaml_input(file1):
+    '''Read in router/switch authentication information from YAML file.'''
     if os.path.isfile(file1):
-        with open(file1) as f1:
-            data1 = yaml.load(f1)
+        with open(file1) as fh1:
+            data1 = yaml.load(fh1)
         return data1
     else:
         sys.exit('Invalid filename {}'.format(file1))
 
 ####################################################################################################
 def node_login(router_auth, telnet_port, telnet_timeout, verbose=False):
+    '''Login to network node and return connection handle.'''
     try:
         if verbose:
             print 'Trying {}...'.format(router_auth['ADDRESS'])
         node_conn = telnetlib.Telnet(router_auth['ADDRESS'], telnet_port, telnet_timeout)
-    except socket.timeout as e:
+    # Omitting as err part since we're not using it:
+    except socket.timeout:
         sys.exit('Connection to {} timed out'.format(router_auth['ADDRESS']))
 
     output = node_conn.read_until(LOGIN_PROMPT, telnet_timeout)
@@ -59,8 +62,9 @@ def node_login(router_auth, telnet_port, telnet_timeout, verbose=False):
     post_login_prompt = [LOGIN_PROMPT, ROUTER_PROMPT]
     if verbose:
         print 'Checking for successful login...'
-    post_login_index, post_login_match, post_login_output = node_conn.expect(post_login_prompt,
-            telnet_timeout*2)
+    # We don't care about/use the match output from expect so assigned to '_'
+    post_login_index, _, post_login_output = node_conn.expect(
+        post_login_prompt, telnet_timeout*2)
     if verbose:
         print 'Post login output:\n{}'.format(post_login_output)
     if post_login_index == 0:
@@ -72,51 +76,54 @@ def node_login(router_auth, telnet_port, telnet_timeout, verbose=False):
     return node_conn
 
 def node_nopaging(node_conn, telnet_timeout, verbose=False):
+    '''Disable terminal paging on network node.'''
     if verbose:
         print 'Disabling paging on node...'
     node_conn.write(ROUTER_NOPAGING + '\n')
-    post_cmd_prompt = [ROUTER_PROMPT]
-    post_cmd_index, post_cmd_match, post_cmd_output = node_conn.expect(post_cmd_prompt,
-            telnet_timeout)
+    output = node_conn.read_until(ROUTER_PROMPT, telnet_timeout)
     if verbose:
-        print 'Node output:\n{}'.format(post_cmd_output)
+        print 'Node output:\n{}'.format(output)
 
 def node_cmd(node_conn, cmd, telnet_timeout, verbose=False):
+    '''Run a command on network node and return its output.'''
     if verbose:
         print 'Sending command {}...'.format(cmd)
     node_conn.write(cmd + '\n')
-    post_cmd_prompt = ['\n']
     # Read until newline sent back - this should be the node echoing back the command
-    post_cmd_index, post_cmd_match, post_cmd_output = node_conn.expect(post_cmd_prompt,
-            telnet_timeout)
+    output = node_conn.read_until('\n', telnet_timeout)
     if verbose:
-        print 'Node echoes back:  {}'.format(post_cmd_output)
-    post_cmd_prompt = [ROUTER_PROMPT]
+        print 'Node echoes back:  {}'.format(output)
     # Read until node prompt, this should indicate command output is done
-    post_cmd_index, post_cmd_match, post_cmd_output = node_conn.expect(post_cmd_prompt,
-            telnet_timeout)
+    output = node_conn.read_until(ROUTER_PROMPT, telnet_timeout)
     # Strip off last line - node prompt
-    lastline = post_cmd_output.rfind('\n')
-    cmd_output = post_cmd_output[:lastline]
+    lastline = output.rfind('\n')
+    cmd_output = output[:lastline]
     if verbose:
-        print 'Node output:\n{}'.format(post_cmd_output)
+        print 'Node output:\n{}'.format(output)
 
     return cmd_output
 
+####################################################################################################
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Connect to a specified router and run a '
-            'command')
+    '''Acquire necessary input options and process node connection, authentication,  command
+    execution and returning output.
+    '''
+    parser = argparse.ArgumentParser(
+        description='Connect to a specified router and run a command')
     parser.add_argument('--version', action='version', version=__version__)
-    parser.add_argument('-v', '--verbose', action='store_true', help='display verbose output',
-            default=False)
-    parser.add_argument('-f', '--file', help='specify YAML file to read router info from',
-            default=ROUTER_FILE)
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', help='display verbose output', default=False)
+    parser.add_argument(
+        '-f', '--file', help='specify YAML file to read router info from', default=ROUTER_FILE)
     args = parser.parse_args()
 
-    router_auth = yaml_input(args.file)
-    node_conn = node_login(router_auth, TELNET_PORT, TELNET_TIMEOUT, args.verbose)
-    node_nopaging(node_conn, TELNET_TIMEOUT, args.verbose)
-    output = node_cmd(node_conn, ROUTER_CMD, TELNET_TIMEOUT, args.verbose)
+    myrouter_auth = yaml_input(args.file)
+    mynode_conn = node_login(myrouter_auth, TELNET_PORT, TELNET_TIMEOUT, args.verbose)
+    node_nopaging(mynode_conn, TELNET_TIMEOUT, args.verbose)
+    myoutput = node_cmd(mynode_conn, ROUTER_CMD, TELNET_TIMEOUT, args.verbose)
 
-    print 'Command output:\n{}'.format(output)
+    print 'Command output:\n{}'.format(myoutput)
+
+    # Finished - cleanup
+    mynode_conn.close()
 
--- template.py
+++ template.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python
 ####################################################################################################
-'''
-<program description>
+'''<program description> - triple quotes should end on this line if on liner...
 '''
 
 # Imports
+# Delete unused lines/comments!
 #import sys
 
 # Globals
 #BASE_FILE = 'file1'
 
+# Metadata
+__author__ = 'James R. Small'
+__contact__ = 'james<period>r<period>small<at>outlook<period>com'
+__date__ = 'Month day, Year'
 __version__ = '0.0.1'
 
 
+#class Myclass1(object):
+    #'''<description>'''
+
+    #__init__(self, param1):
+    #    '''<description>'''
+    #    self.param1 = param1
+
+    #method1(self, param1):
+    #    '''<description>'''
+    #    pass
+
 #def func1(param1):
+    '''<description>'''
+    # Do stuff...
+
+def main():
+    '''<description>'''
     # Do stuff...
 
 if __name__ == '__main__':
-    # Do stuff...
+    '''Call main and put all logic there per best practices.'''
+    sys.exit(main(sys.argv[1:]) or 0)
 
 
 ####################################################################################################
@@ -25,4 +46,8 @@ if __name__ == '__main__':
 #
 # pylint <script>.py
 #   Score should be >= 8.0
+#
+# Future:
+# * Testing - doctest/unittest/other
+# * Logging
 #
