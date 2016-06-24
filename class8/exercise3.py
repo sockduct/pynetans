892504c2fa9432947513f3998ab6fd3026098184
James Small <james.r.small@outlook.com>
2016-06-24 14:09:25 -0700

    Fixing symbolic link to non-GitHub accessible file - now real file is in repo, other directory has symbolic link to repo file

--- class8/exercise3.py
+++ class8/exercise3.py
@@ -1,1 +1,26 @@
-/home/jsmall/DJANGOX/djproject/net_system/add_temp_devs.py
+#!/usr/bin/env python
+
+from net_system.models import NetworkDevice
+import django
+
+django.setup()
+
+# Variable name can't use a hyphen! ('-')
+temp_rtr3 = NetworkDevice(
+    device_name='temp-rtr3',
+    device_type='cisco_ios',
+    ip_address='192.0.2.13',
+    port=22,
+)
+# If you don't save it, it's not retained!!!
+temp_rtr3.save()
+print 'temp-rtr3:  {}'.format(temp_rtr3)
+
+temp_rtr4 = NetworkDevice.objects.get_or_create(
+    device_name='temp-rtr4',
+    device_type='cisco_ios',
+    ip_address='192.0.2.14',
+    port=22,
+)
+print 'temp-rtr4:  {}'.format(temp_rtr4)
+
